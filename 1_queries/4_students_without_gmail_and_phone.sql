fbdad696e2bb56b24619f1f62284ad4680b219c7
“MahnazEs” <esmaeilymahnaz@gmail.com>
2023-03-06 16:55:03 -0800

    added some query for students

---
+++ 1_queries/4_students_without_gmail_and_phone.sql
@@ -0,0 +1,11 @@
+/* SELECT name, email, cohort_id
+FROM students
+WHERE email !='%gmail.com'
+AND phone IS NULL; */
+
+
+
+SELECT name, id, email, cohort_id
+FROM students
+WHERE email NOT LIKE '%gmail.com'
+AND phone IS NULL;
